833b49773ef91dae14ecd730e6cdd33c0a5b6eb4
IrinaSpasova <77418931+IrinaSpasova@users.noreply.github.com>
2021-03-13 21:08:01 +0200

    Add files via upload

---
+++ SoftUni/Programming Basics/02/02. Greater Number.js
@@ -0,0 +1,15 @@
+function data(input){
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+/*LOGIC*/
+
+if (a>b){
+    console.log(a);
+}else{
+    console.log(b);
+}
+
+}
+
+data(["5", "32"]);

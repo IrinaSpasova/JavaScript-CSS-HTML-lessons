daef732b27f1c3726764e718303793ab6415f330
IrinaSpasova <77418931+IrinaSpasova@users.noreply.github.com>
2021-03-17 17:37:01 +0200

    Update 08. Scholarship.js

--- SoftUni/Programming Basics/02/Exercise/08. Scholarship.js
+++ SoftUni/Programming Basics/02/Exercise/08. Scholarship.js
@@ -1,3 +1,6 @@
+/*************************************************************************************************/
+/*     Грешно решение, което Джъдж приема за вярно - не отчита възможност за равни стипендии     */
+/*************************************************************************************************/
 function data(input) {
     let income = Number(input[0]);
     let avergeAssessments = Number(input[1]);
@@ -32,3 +35,50 @@ function data(input) {
 data(["6880.00",
 "5.80",
 "6000.00"])
+
+
+
+/******************************************************************************************/
+/*             Вярно решение, което Джъдж приема за грешно                                */
+/******************************************************************************************/
+
+function data(input) {
+    let income = Number(input[0]);
+    let avergeAssessments = Number(input[1]);
+    let minimalSalary = Number(input[2]);
+    let scholarship = Math.floor(avergeAssessments * 25);
+    let socialScolarship = Math.floor(minimalSalary * 0.35);
+
+    /*LOGIC*/
+
+    if (avergeAssessments <= 4.5) {
+        console.log("You cannot get a scholarship!");
+    } else if (minimalSalary <= income && avergeAssessments < 5.5) {
+        console.log("You cannot get a scholarship!");
+    }
+
+
+    if (avergeAssessments > 4.5 && avergeAssessments < 5.5 && income < minimalSalary) {
+        console.log(`You get a Social scholarship ${(socialScolarship)} BGN`);
+    }
+
+    if (avergeAssessments >= 5.5 && income < minimalSalary && scholarship > socialScolarship) {
+        console.log(`You get a scholarship for excellent results ${(scholarship)} BGN`);
+    }
+    if (avergeAssessments >= 5.5 && income < minimalSalary && scholarship < socialScolarship) {
+        console.log(`You get a Social scholarship ${(socialScolarship)} BGN`);
+    }else if (avergeAssessments >= 5.5 && income < minimalSalary && socialScolarship <= scholarship){
+        console.log(`You get a scholarship for excellent results ${(scholarship)} BGN`);
+    }
+    if (avergeAssessments >= 5.5 && income >= minimalSalary) {
+        console.log(`You get a scholarship for excellent results ${(scholarship)} BGN`);
+    }
+
+}
+
+
+
+Проверка : data(["300",
+    "5.60",
+    "400"
+])  (това са === стипендии)

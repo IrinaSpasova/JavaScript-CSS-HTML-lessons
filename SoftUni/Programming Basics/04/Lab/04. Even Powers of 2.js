33012fa22126dd4efb880c395929113a1f212892
IrinaSpasova <77418931+IrinaSpasova@users.noreply.github.com>
2021-03-27 18:15:33 +0200

    Add files via upload

---
+++ SoftUni/Programming Basics/04/Lab/04. Even Powers of 2.js
@@ -0,0 +1,10 @@
+function data(input){
+    let n = Number(input[0]);
+    for(let i=0; i<=n; i+=2){
+        let result = Math.pow(2,i);
+        console.log(result);
+    }
+        
+   
+}
+data(["3"]);

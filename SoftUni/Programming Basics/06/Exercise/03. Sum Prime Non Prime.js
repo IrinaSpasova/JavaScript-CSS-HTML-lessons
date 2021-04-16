bded468385b4a1f0a6e4f73bc0bfeabd67a2770c
IrinaSpasova <77418931+IrinaSpasova@users.noreply.github.com>
2021-04-16 08:57:39 +0300

    Update 03. Sum Prime Non Prime.js

--- SoftUni/Programming Basics/06/Exercise/03. Sum Prime Non Prime.js
+++ SoftUni/Programming Basics/06/Exercise/03. Sum Prime Non Prime.js
@@ -1,6 +1,40 @@
-function data(input) {
+function test(input) {
+ 
+    let index = 0;
+    let primeNums = 0;
+    let nonPrimeNums = 0;
+ 
+    while (input[index] != "stop") {
+        let num = Number(input[index]);
+        if (num < 0) {
+            console.log("Number is negative.");
+        } else {
+            let isPrime = true;
+            for (let i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primeNums += num;
+            } else {
+                nonPrimeNums += num;
+            }
+        }
+        index++
+ 
+    }
+    console.log(`Sum of all prime numbers is: ${primeNums}`);
+    console.log(`Sum of all non prime numbers is: ${nonPrimeNums}`);
+ 
+}
 
-    /*LOGIC*/
+
+
+/*function data(input) {
+
+   
     let counter = 0;
     let primeNum = 0;
     let nonPrimeNum = 0;
@@ -51,7 +85,7 @@ data(["30",
 "33",
 "-1",
 "20",
-"stop"]);
+"stop"]);*/
 
 /*function data(input) {
 

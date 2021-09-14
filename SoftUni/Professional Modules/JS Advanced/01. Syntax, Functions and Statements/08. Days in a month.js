24c21f4919385c64655ce3bab6f432d7ddb87042
IrinaSpasova <77418931+IrinaSpasova@users.noreply.github.com>
2021-09-14 20:48:05 +0300

    Update 08. Days in a month.js

--- SoftUni/Professional Modules/JS Advanced/01. Syntax, Functions and Statements/08. Days in a month.js
+++ SoftUni/Professional Modules/JS Advanced/01. Syntax, Functions and Statements/08. Days in a month.js
@@ -1,4 +1,12 @@
+/*8. Days in a month
+Write a JavaScript function to get the number of days in a month.
+The input comes as two numeric parameters. The first element is the month, the second is the year.
+The output must return the number of days in a month for a given year.*/
+
 function daysMonth(num1, num2) {
     let date = new Date(num2, num1, 0).getDate()
     console.log(date);
 }
+
+daysMonth(1,2012);
+

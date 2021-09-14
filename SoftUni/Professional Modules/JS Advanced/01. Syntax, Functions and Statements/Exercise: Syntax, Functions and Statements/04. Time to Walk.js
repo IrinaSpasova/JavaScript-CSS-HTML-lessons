f4c18ede19d7e09c5ff9725ebce7c0a115a0fe20
IrinaSpasova <77418931+IrinaSpasova@users.noreply.github.com>
2021-09-14 20:50:51 +0300

    Update 04. Time to Walk.js

--- SoftUni/Professional Modules/JS Advanced/01. Syntax, Functions and Statements/Exercise: Syntax, Functions and Statements/04. Time to Walk.js
+++ SoftUni/Professional Modules/JS Advanced/01. Syntax, Functions and Statements/Exercise: Syntax, Functions and Statements/04. Time to Walk.js
@@ -36,3 +36,24 @@ data(2564, 0.70, 5.5)
 
     return n.toTimeString().slice(0, 8)
 }*/
+
+
+
+ 
+
+
+                                                   padStart
+
+/*console.log(str1.padStart(2, '0'));
+// expected output: "05"
+
+const fullNumber = '2034399002125581';
+const last4Digits = fullNumber.slice(-4);
+const maskedNumber = last4Digits.padStart(fullNumber.length, '*');
+
+console.log(maskedNumber);
+// expected output: "************5581"*/
+
+
+
+
